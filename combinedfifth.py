c65778e4a5942d88a4a2e89b7654191d41fbae77
Ravi Prasai <raviprasai1994@gmail.com>
2021-02-22 15:39:24 -0600

    origin

---
+++ combinedfifth.py
@@ -0,0 +1,51 @@
+import csv
+from datetime import datetime
+
+open_file = open("death_valley_2018_simple.csv", "r")
+csv_file = csv.reader(open_file, delimiter=",")
+
+header_row = next(csv_file)
+
+for index, column_header in enumerate(header_row):
+    print("Index:", index, "Column Name:", column_header)
+
+highs = []
+dates = []
+lows = []
+
+# Exmaple
+# somedate = "2018-07-01"
+# converted_date = datetime.strptime(somedate, "%Y-%m-%d")
+# print(converted_date)
+
+for row in csv_file:
+    try:
+        high = int(row[4])
+        low = int(row[5])
+        converted_date = datetime.strptime(row[2], "%Y-%m-%d")
+    except ValueError:
+        print(f"Missing data for {converted_date}")
+    else:
+        highs.append(int(row[4]))
+        lows.append(int(row[5]))
+        dates.append(converted_date)
+
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+
+plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
+
+# filling the color between the lines
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+plt.title("Daily high temperatures, 2018", fontsize=16)
+plt.xlabel("", fontsize=12)
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis="both", labelsize=12)
+
+
+fig.autofmt_xdate()
+plt.show()
